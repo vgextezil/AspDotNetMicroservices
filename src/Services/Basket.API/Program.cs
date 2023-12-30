b754d7f9c182bda82e2b5a815dd75b6682b543e8
vgextezil <hakman.vasyl@chnu.edu.ua>
2023-12-30 15:56:14 +0200

    added redis all works without docker

---
+++ src/Services/Basket.API/Program.cs
@@ -0,0 +1,34 @@
+using Basket.API.Repositories;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(options =>
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" }));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json","Basket.API v1"));
+
+}
+
+//app.UseHttpsRedirection();
+app.UseRouting();
+app.UseRouting().UseEndpoints(endpoint => endpoint.MapControllers());
+
+app.Run();
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,8 @@
-using System.Reflection;
 using Catalog.API.Data;
 using Catalog.API.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
