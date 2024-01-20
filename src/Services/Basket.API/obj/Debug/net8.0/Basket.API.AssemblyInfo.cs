49a9e51d5119287d04a1ed1851f7a2082b715046
vgextezil <hakman.vasyl@chnu.edu.ua>
2024-01-20 09:04:00 +0200

    message

--- src/Services/Basket.API/obj/Debug/net8.0/Basket.API.AssemblyInfo.cs
+++ src/Services/Basket.API/obj/Debug/net8.0/Basket.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b754d7f9c182bda82e2b5a815dd75b6682b543e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
